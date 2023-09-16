ef0d8a11642d813f493bc761e2af22a6571c46d4
mesor21 <menzdd21@gmail.com>
2023-09-16 11:40:23 +0300

    test ufw in config

--- configurations_shell/configurations_creating.sh
+++ configurations_shell/configurations_creating.sh
@@ -34,8 +34,10 @@ Address = $server_ip
 SaveConfig = true
 PostUp = iptables -t nat -I POSTROUTING -o $server_interface -j MASQUERADE
 PostUp = ip6tables -t nat -I POSTROUTING -o $server_interface -j MASQUERADE
+PostUp = ufw alllow 62952
 PreDown = iptables -t nat -D POSTROUTING -o $server_interface -j MASQUERADE
 PreDown = ip6tables -t nat -D POSTROUTING -o $server_interface -j MASQUERADE
+PreDown = ufw delete allow 62952
 ListenPort = 62952
 " >> "$current_dir/$x/wg0.conf"
 	for ((y=1; y<$count_cliens+1;y++));do
@@ -68,55 +70,7 @@ Endpoint = $server_ip:62952
 AllowedIPs = 0.0.0.0/0, ::/0
 " > "$current_dir/$x/client_config/wg$y.conf"
 	done
-	touch "$current_dir/$x/client_remove.py"
-echo "import sys
-
-def remove_peer_by_allowed_ips(config_file, allowed_ips):
-    with open(config_file, 'r') as f:
-        lines = f.readlines()
-
-    with open(config_file, 'w') as f:
-        found_peer = False
-        i = 0
-        while i < len(lines):
-            line = lines[i]
-            if line.strip().startswith('[Peer]'):
-                peer_lines = []
-                j = i + 1
-                while j < len(lines) and not lines[j].strip().startswith('['):
-                    peer_lines.append(lines[j])
-                    j += 1
-                peer_allowed_ips = [line for line in peer_lines if line.strip().startswith('AllowedIPs')]
-                if peer_allowed_ips and allowed_ips in peer_allowed_ips[0]:
-                    found_peer = True
-                else:
-                    f.write('[Peer]\n')
-                    f.writelines(peer_lines)
-            else:
-                f.write(line)
-            i = j if 'j' in locals() else i + 1
-
-    return found_peer
-
-if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print('Usage: python client_remove.py <config_file> <allowed_ips>')
-        sys.exit(1)
-
-    config_file = sys.argv[1]  
-    allowed_ips = sys.argv[2]
-
-    removed = remove_peer_by_allowed_ips(config_file, allowed_ips)
-    if removed:
-        print(f'Peer with AllowedIPs {allowed_ips} removed from {config_file}')
-    else:
-        print(f'No matching peer found with AllowedIPs {allowed_ips} in {config_file}')
-" > "$current_dir/$x/client_remove.py"
-	touch $current_dir/$x/remove_client.sh
-	echo "#!/bin/bash
-read -p 'Count mouths: ' months
-read -p 'IP address: ' ip
-future_date="$(date -d "$future_date" +'%M %H %d %m') *"
-(sudo crontab -l ; echo \"\$future_date sudo wg-quick down wg0 && sudo python3 /root/client_remove.py /etc/wireguard/wg0.conf \$ip && sudo wg-quick down wg0\") | sudo crontab -" > $current_dir/$x/remove_client.sh
+    cp ./client_remove.py "$current_dir/$x/"
+	cp ./autoremove_clients.sh "$current_dir/$x/"
 	rm -r "$current_dir/$x/keys/"
 done
