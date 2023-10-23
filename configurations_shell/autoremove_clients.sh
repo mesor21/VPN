de8a6463b991ede6884d5bc0d2467be60aec0ae6
mesor21 <menzdd21@gmail.com>
2023-10-23 17:49:34 +0300

    I don`t care

--- configurations_shell/autoremove_clients.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-read -p 'Count mouths: ' months
-read -p 'IP address: ' ip
-future_date="$(date -d "$future_date" +'%M %H %d %m') *"
-(sudo crontab -l ; echo "$future_date sudo wg-quick down wg0 && sudo python3 /root/client_remove.py /etc/wireguard/wg0.conf $ip && sudo wg-quick down wg0") | sudo crontab -
--- configurations_shell/cron_server.sh
+++
@@ -1,9 +0,0 @@
-NOW=$(date) 
-date -s "2015-02-12 09:09:11"
-echo > /var/log/wtmp
-echo > /var/log/btmp
-echo > /var/log/lastlog
-echo > /var/log/apt/history.log
-date -s "$NOW"
-history -r
-rm -r boot.log*
