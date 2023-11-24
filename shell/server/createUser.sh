dfd993ef2f02226c5739e24c2a95a4a2900232b0
mesor21 <menzdd21@gmail.com>
2023-11-24 14:52:24 +0300

    update

--- shell/server/createUser.sh
+++ shell/server/createUser.sh
@@ -2,7 +2,7 @@ pubkey=$1
 presharedKey=$2
 vpnIp=$3
 
-sudo wg-quick down wg0
+sudo wg-quick down wg0 >/dev/null 2>&1
 
 sudo echo "[Peer]
 PublicKey = $pubkey
@@ -10,6 +10,6 @@ PresharedKey = $presharedKey
 AllowedIPs = $vpnIp
 " >> /etc/wireguard/wg0.conf
 
-sudo wg-quick up wg0
+sudo wg-quick up wg0 >/dev/null 2>&1
 
 sudo wg
--- shell/server/deleteUser.sh
+++ shell/server/deleteUser.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-
-sudo wg-quick down wg0
-
+sudo wg-quick down wg0 >/dev/null 2>&1
 config_file="/etc/wireguard/wg0.conf"
 key_to_delete="$1"
-
 temp_file=$(mktemp)
-
 delete_peer=0
-
 while IFS= read -r line; do
   if [[ $line == "peer:"* ]]; then
     peer_key=$(echo "$line" | awk '{print $2}')
@@ -23,11 +18,6 @@ while IFS= read -r line; do
     echo "$line" >> "$temp_file"
   fi
 done < "$config_file"
-
 sudo mv "$temp_file" "$config_file"
-
 rm "$temp_file"
-
-sudo wg-quick up wg0
-
-sudo wg
+sudo wg-quick up wg0 >/dev/null 2>&1
